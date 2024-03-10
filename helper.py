5efb01903aab47e7fe32456da28df56f91b029e3
Armand Soto <armand.e.soto@gmail.com>
2024-03-10 16:05:24 -0400

    added helper.py

---
+++ helper.py
@@ -0,0 +1,5 @@
+def print_board(board):
+    for row in board:
+        for elem in row:
+            print(elem, end=' ')
+        print('\n')
--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-
+import helper
+print_board(board_state)
 # init board state
 rows = cols = 3
+
 characters = ('O', 'X')
 board_state = [[i * cols + j for j in range(cols)] for i in range(rows)]
-# write the rules of the game
-print("Each position is labeled as follows:")    
 
+# write the rules of the game
+print("Each position is labeled as follows: ")
+    
 #print board state
-for row in board_state:
-    for elem in row:
-        print(elem, end=' ')
-    print('\n')
+
+
+print("Then select either X or O")
+
+board_state = [['-' for _ in range(cols)] for _ in range(rows)]
+
+
+print (sys.path)
+
+
+
+
+
 
 # now init board state to '-'
 
